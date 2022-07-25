334a9c0804a86ef83d9968dea1befd0ee8e2eddb
BulBUl-Ahmed <mdbulbulmolla1222@gmail.com>
2022-07-25 18:13:28 +0600

    added storage api in javascript

--- xhr.js
+++ xhr.js
@@ -53,32 +53,94 @@ console.clear();
 
 //promise
 
-const makeRequest = (method, url, data) => {
-  return new Promise((resolve, reject) => {
+// const makeRequest = (method, url, data) => {
+//   return new Promise((resolve, reject) => {
 
-    const xhr = new XMLHttpRequest();
-    xhr.open(method, url);
+//     const xhr = new XMLHttpRequest();
+//     xhr.open(method, url);
 
-    xhr.setRequestHeader("content-type", "application/json");
+//     xhr.setRequestHeader("content-type", "application/json");
 
-    xhr.onload = () => {
-      let data = xhr.response;
-      console.log(JSON.parse(data));
-    };
+//     xhr.onload = () => {
+//       let data = xhr.response;
+//       console.log(JSON.parse(data));
+//     };
   
-    xhr.onerror = () => {
-      console.log("error is here");
-    };
+//     xhr.onerror = () => {
+//       console.log("error is here");
+//     };
 
-    xhr.send(JSON.stringify(data));
-  })
-};
+//     xhr.send(JSON.stringify(data));
+//   })
+// };
 
-const getData = () => {
-  makeRequest("GET", "https://jsonplaceholder.typicode.com/posts")
-  .then((res) => console.log(res))
-};
+// const getData = () => {
+//   makeRequest("GET", "https://jsonplaceholder.typicode.com/posts")
+//   .then((res) => console.log(res))
+// };
 
-getData()
+// getData()
+
+
+//========================= fetch api =============================
+
+// fetch("https://jsonplaceholder.typicode.com/posts/101")
+
+// .then((res) => {
+//   if(!res.ok) {
+//     const message = `Error: ${res.status}`
+//     throw new Error(message)
+//   }
+// })
+// .then((res) => console.log(res))
+// .catch((err) => console.log(err))
+
+
+//==================web storage api | localStorage, sessionStorage. cookie
+
+// ===========localStorage===========
+
+// web storage api --- allows us to store & read data in browser 
+// web storage api --- localStorage, sessionStorage
+
+// localStorage -- store , read , update and remove data
+// no expire date: data never gets lost even if you close the browser
+
+// localStorage store data as key value pain -- string
+//setItem(key, value)
+
+localStorage.setItem("userName", "Bulbul Ahmed")
+localStorage.setItem("password", "01456565687")
+
+const userName =localStorage.getItem("userName")
+const userPassword = localStorage.getItem("password")
+
+console.log(userName, userPassword);
+
+
+localStorage.setItem("userName", "rabeya islam")
+ localStorage.setItem("password", "4547154")
+
+//removeItem(key)
+
+localStorage.removeItem("userName")
+localStorage.removeItem("password")
+
+const countries = ["Australia","Bangladesh","Nepal"]
+
+localStorage.setItem("countries", JSON.stringify(countries))
+localStorage.setItem("password", "014575245")
+
+const country = JSON.parse(localStorage.getItem("countries"))
+const pass = localStorage.getItem("password")
+
+console.log(country, pass);
+
+
+
+//===========session storage=============
+
+
+
 
 
